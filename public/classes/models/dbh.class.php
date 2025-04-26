68b452f73655e1152303f8ac9cc6f728d194a0c1
Zorbiks <176812563+Zorbiks@users.noreply.github.com>
2025-04-26 22:32:16 +0100

    Replace hardcoded database credentials with environment variables in .htaccess

--- public/classes/models/dbh.class.php
+++ public/classes/models/dbh.class.php
@@ -3,14 +3,15 @@
 class Dbh {
     protected function connect() {
         try {
-            $dbusername = "root";
-            $dbpassword = "";
-            $host = "localhost";
-            $dbname = "thesebm";
+            echo $_SERVER["DN_HOST"];
+            $db_host = $_SERVER["DB_HOST"];
+            $db_username = $_SERVER["DB_USERNAME"];
+            $db_password = $_SERVER["DB_PASSWORD"];
+            $db_name = $_SERVER["DB_NAME"];
 
 
-            $dsn = "mysql:host=$host;dbname=$dbname";
-            $dbh = new PDO($dsn, $dbusername, $dbpassword);
+            $dsn = "mysql:host=$db_host;dbname=$db_name";
+            $dbh = new PDO($dsn, $db_username, $db_password);
             $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
             return $dbh;
