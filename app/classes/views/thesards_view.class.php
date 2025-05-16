2def75731822c36e76fc2f2eea2d05d5ee9b3618
Zorbiks <176812563+Zorbiks@users.noreply.github.com>
2025-05-16 20:41:12 +0100

    complete Thesards page with publication counts and view buttons

---
+++ app/classes/views/thesards_view.class.php
@@ -0,0 +1,42 @@
+<?php
+
+class ThesardView extends ThesardsModel {
+    public function renderThesardsTable() {
+
+        $thesards = $this->getAllThesards();
+
+        // echo print_r($thesards);
+
+        ?>
+            <div class="table-responsive">
+                <table class="table table-striped text-nowrap">
+                    <thead>
+                        <tr>
+                            <th class="bg-primary text-light">Prénom</th>
+                            <th class="bg-primary text-light">Nom</th>
+                            <th class="bg-primary text-light">E-mail</th>
+                            <th class="bg-primary text-light">Nombre des publication</th>
+                            <th class="bg-primary text-light">Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody class="align-middle">
+                        <?php foreach($thesards as $thesard): ?>
+                            <tr>
+                                <td><?=$thesard["prenom"]?></td>
+                                <td><?=$thesard["nom"]?></td>
+                                <td><?=$thesard["email"]?></td>
+                                <td><?=$this->getNumberOfPublicationByThesardId($thesard["id"])?></td>
+                                <td>
+                                    <a class="btn btn-primary" href="publications.php?action=search&search=<?=$thesard["prenom"] . " " . $thesard["nom"] ?>&filter=thesard">
+                                        <i class="fa-solid fa-eye fa-fw"></i>
+                                        Voir les publication
+                                    </a>
+                                </td>
+                            </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+            </div>
+        <?php
+    }
+}
